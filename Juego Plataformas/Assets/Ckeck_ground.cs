588626591eb58c667fd17ee2f5aa295f6350032f
Robert Gonzalez <robert_gon97@hotmail.com>
2018-04-19 17:48:33 -0400

    modificacion de animacion de juego de plataformas

---
+++ Juego Plataformas/Assets/Ckeck_ground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ckeck_ground : MonoBehaviour {
+
+	private Player_controller player;//creamos un stancia del objeto Player_controller osea el script
+
+	// Use this for initialization
+	void Start () {
+		player=GetComponentInParent<Player_controller> ();//busacamos el componete pero en este caso del padre
+		//y asedemos buscando el script Player_controller
+
+	}
+	//funcion q es cuando entramos en una colicion
+	void OnCollisionStay2D(Collision2D col){
+		if(col.gameObject.tag=="Ground"){
+			player.ground = true;//cambiamos el valor del Gruond
+		}
+
+	}
+	//funcion q es cuando salimos en una colicion
+	void OnCollisionExit2D(Collision2D col){
+		if(col.gameObject.tag=="Ground"){
+			player.ground = false;//cambiamos el valor del Gruond
+		}
+	}
+}
--- Juego Plataformas/Assets/Player_controller.cs
+++ Juego Plataformas/Assets/Player_controller.cs
@@ -8,16 +8,23 @@ public class Player_controller : MonoBehaviour {
 
 	public float speed = 2f;
 
-	private Rigidbody2D rig2d;//caturamos el componente Rigidbody en una varible 
+	public bool ground;
+
+	private Rigidbody2D rig2d;//caturamos el componente Rigidbody en una varible
+	private Animator anim;//caturamos el componente Animator en una varible
 	// Use this for initialization
 	void Start () {
 		rig2d = GetComponent<Rigidbody2D> ();//detetamos el componente rigidbody al inciar el jugo
+		//de nnuestro personaje
+		anim = GetComponent<Animator> ();//detetamos el componente Animator al inciar el jugo
 		//de nnuestro personaje
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		anim.SetFloat ("Speed",Mathf.Abs(rig2d.velocity.x));//anim.SetFloat envia al animator 
+		//Mathf.Abs obtine el valor absoluto de un numero
+		anim.SetBool("Ground",ground);
 	}
 
 
@@ -27,15 +34,26 @@ public class Player_controller : MonoBehaviour {
 
 		rig2d.AddForce (Vector2.right * speed * h);//se le anñane un vector de 2 dimensiones q vaya a la derecha
 		//despues se le multiplica por la velocidad q tenemos guardada y por la direcion
-
+		/*
 		//rectificamos si nos movemos mas rapido q la velocidad maxima en el eje horizontal y acipnamos la nueva velocidad
 		if(rig2d.velocity.x > maxSpeed ){
 			rig2d.velocity = new Vector2 (maxSpeed , rig2d.velocity.y);
 		}
 		if(rig2d.velocity.x < -maxSpeed ){
 			rig2d.velocity = new Vector2 (-maxSpeed , rig2d.velocity.y);
-		}
+		}*/
+		//otra manera
+		float limitSpeed = Mathf.Clamp (rig2d.velocity.x,-maxSpeed,maxSpeed);
+		rig2d.velocity = new Vector2 (limitSpeed , rig2d.velocity.y);
 
+
+		if(h > 0.1f){
+			transform.localScale = new Vector3 (1f,1f,1f);//acedemos a la localizacion de la escala y le 
+			//le cremos un nuevo vector para cambiarsela
+		}
+		if(h < -0.1f){
+			transform.localScale = new Vector3 (-1f,1f,1f);
+		}
 		Debug.Log (rig2d.velocity.x);
 
 
